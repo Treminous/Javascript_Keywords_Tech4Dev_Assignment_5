ad9f0bd7c6e2a4e36b29036f4542095fdd2df0ae
Milka Bakhuya <bakhuyamilka@gmail.com>
2023-05-19 08:16:17 +0300

    Created a break and while loop code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//The switch statement allows to make a decision from the number of choices.
 function marksgrade()
 {
 grade = document.form1.text1.value;
@@ -27,3 +28,49 @@ console.log("Wrong grade.........");
 }
 }
 marksgrade();
+//The break statement is used to terminate a loop, switch or label statement.
+function w3rfunction(){
+    'use strict';
+    var x = document.getElementById("number").value;
+    if(x < 2 || x > 10){
+    document.getElementById("result").innerHTML = "The number is not within the range.";
+    }
+    else
+    {
+    var z = 0;
+    var y;
+    for(y=1; y<=x; y++)
+    {
+    z = z + y;
+    if(y===x)
+    {
+    break;
+    }
+    }
+    document.getElementById("result").innerHTML = 'The sum of numbers between  1 to '+ x +' is: '+ z;
+    }
+    }
+    //The while loop is to execute a statement or code block repeatedly as long as an expression is true. Once the expression becomes false, the loop terminates.
+    var x = 1;
+var y = 0;
+var z = 0;
+document.getElementById("result").innerHTML = "List of numbers : ";
+while (x <=10 )
+{
+z = x %  2;
+if (z !== 0)
+{
+var newParagraph1 = document.createElement("p");
+var newText1 = document.createTextNode(x);
+newParagraph1.appendChild(newText1);
+document.body.appendChild(newParagraph1);
+y=y+x;
+}
+x++;
+}
+var newParagraph1 = document.createElement("p");
+var newText1 = document.createTextNode("The sum of even numbers between 0 to 10 is : " + y);
+newParagraph1.appendChild(newText1);
+document.body.appendChild(newParagraph1);
+
+    
